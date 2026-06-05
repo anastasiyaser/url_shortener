54a4c696f1e2acdec34d23a5489e8f327e65a208
AnastasiyaSerebryakova <anastasiya.serebryakova@host.com>
2026-06-06 01:49:00 +0200

    Add CRUD for Tag entity

--- app/src/Repository/TagRepository.php
+++ app/src/Repository/TagRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Tag;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -11,8 +12,53 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class TagRepository extends ServiceEntityRepository
 {
+    /**
+     * Items per page.
+     *
+     * Use constants to define configuration options that rarely change instead
+     * of specifying them in configuration files.
+     * See https://symfony.com/doc/current/best_practices.html#configuration
+     *
+     * @var int
+     */
+    public const PAGINATOR_ITEMS_PER_PAGE = 10;
+
+    /**
+     * Constructor.
+     *
+     * @param ManagerRegistry $registry Manager registry
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Tag::class);
     }
+
+    /**
+     * @return QueryBuilder
+     */
+    public function queryAll(): QueryBuilder
+    {
+        return $this->createQueryBuilder('tag');
+    }
+    /**
+     * Save entity.
+     *
+     * @param Tag $category Tag entity
+     */
+    public function save(Tag $tag): void
+    {
+        $this->getEntityManager()->persist($tag);
+        $this->getEntityManager()->flush();
+    }
+    /**
+     * Delete entity.
+     *
+     * @param Tag $tag Tag entity
+     */
+    public function delete(Tag $tag): void
+    {
+        $this->getEntityManager()->remove($tag);
+        $this->getEntityManager()->flush();
+    }
 }
+
---
+++ app/src/Service/TagService.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Tag service.
+ */
+
+namespace App\Service;
+
+use App\Entity\Tag;
+use App\Repository\TagRepository;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+
+/**
+ * Class TagService.
+ */
+class TagService implements TagServiceInterface
+{
+    /**
+     * Items per page.
+     *
+     * Use constants to define configuration options that rarely change instead
+     * of specifying them in app/config/config.yml.
+     * See https://symfony.com/doc/current/best_practices.html#configuration
+     *
+     * @constant int
+     */
+    private const PAGINATOR_ITEMS_PER_PAGE = 10;
+
+    /**
+     * Constructor.
+     *
+     * @param TagRepository     $tagRepository Tag repository
+     * @param PaginatorInterface $paginator      Paginator
+     */
+    public function __construct(private readonly TagRepository $tagRepository, private readonly PaginatorInterface $paginator)
+    {
+    }
+
+    /**
+     * Get paginated list.
+     *
+     * @param int $page Page number
+     *
+     * @return PaginationInterface Paginated list
+     */
+    public function getPaginatedList(int $page): PaginationInterface
+    {
+        return $this->paginator->paginate(
+            $this->tagRepository->queryAll(),
+            $page,
+            self::PAGINATOR_ITEMS_PER_PAGE,
+            [
+                'sortFieldAllowList' => ['tag.id', 'tag.createdAt'],
+                'defaultSortFieldName' => 'tag.createdAt',
+                'defaultSortDirection' => 'desc',
+            ]
+        );
+    }
+
+    /**
+     * Save entity.
+     *
+     * @param Tag $tag Tag entity
+     */
+    public function save(Tag $tag): void
+    {
+        if (null === $tag->getId()) {
+            $tag->setCreatedAt(new \DateTimeImmutable());
+        }
+        $this->tagRepository->save($tag);
+    }
+
+    /**
+     * @param Tag $tag
+     * @return void
+     */
+    public function delete(Tag $tag): void
+    {
+        $this->tagRepository->delete($tag);
+    }
+}
